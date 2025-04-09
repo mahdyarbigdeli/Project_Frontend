34296fde3cae9443265b05fb653415d756e1b43f
a.ghahari <alighahari1365@gmail.com>
2025-04-09 19:35:54 +0330

    fix

--- src/components/PayPal/PayPalButton.tsx
+++ src/components/PayPal/PayPalButton.tsx
@@ -1,34 +1,35 @@
 "use client";
 
-import { PayPalButtons } from "@paypal/react-paypal-js";
+// import { PayPalButtons } from "@paypal/react-paypal-js";
 
 export default function PayPalButton() {
-  return (
-    <PayPalButtons 
-      createOrder={(props, actions) => {
-        return actions.order
-          .create({
-            purchase_units: [
-              {
-                amount: {
-                  currency_code: "USD",
-                  value: "599",
-                },
-                items: [
-                  {
-                    name: "Lifetime Subscription",
-                    quantity: "1",
-                    unit_amount: { currency_code: "USD", value: "599" },
-                  },
-                ],
-              },
-            ],
-            intent: "CAPTURE",
-          })
-          .then((err) => {
-            console.log("error => ", err);
-            return err;
-          });
-      }}></PayPalButtons>
-  );
+  return;
+  // return (
+  //   <PayPalButtons 
+  //     createOrder={(props, actions) => {
+  //       return actions.order
+  //         .create({
+  //           purchase_units: [
+  //             {
+  //               amount: {
+  //                 currency_code: "USD",
+  //                 value: "599",
+  //               },
+  //               items: [
+  //                 {
+  //                   name: "Lifetime Subscription",
+  //                   quantity: "1",
+  //                   unit_amount: { currency_code: "USD", value: "599" },
+  //                 },
+  //               ],
+  //             },
+  //           ],
+  //           intent: "CAPTURE",
+  //         })
+  //         .then((err) => {
+  //           console.log("error => ", err);
+  //           return err;
+  //         });
+  //     }}></PayPalButtons>
+  // );
 }
