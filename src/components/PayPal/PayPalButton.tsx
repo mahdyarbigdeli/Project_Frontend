d3c18778be1df7ab427b5212544c8a0fe64b89d7
Nasrin Mokhtari <nasrin.mokhtari319@gmail.com>
2025-05-16 20:56:40 +0330

    add approve

--- src/components/PayPal/PayPalButton.tsx
+++ src/components/PayPal/PayPalButton.tsx
@@ -1,20 +1,35 @@
+"use client";
+
+import { useEffect } from "react";
 import {
-  ICreateOrder,
-  PayPalCreateOrderAPI,
-} from "@/services/paypal/paypal.services";
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
-const NEXT_PUBLIC_APP_PAYPAL_CLIENT_ID = process.env
-  .NEXT_PUBLIC_APP_PAYPAL_CLIENT_ID as string;
+  PayPalScriptProvider,
+  PayPalButtons,
+} from "@paypal/react-paypal-js";
+import { ICreateOrder, PayPalCreateOrderAPI } from "@/services/paypal/paypal.services";
 
 const PayPalCheckout = (data: ICreateOrder) => {
+  const NEXT_PUBLIC_APP_PAYPAL_CLIENT_ID = process.env
+    .NEXT_PUBLIC_APP_PAYPAL_CLIENT_ID as string;
+
+  // useEffect(() => {
+  //   // اگر PayPal قبلاً لود شده باشه، پاکش کن
+  //   const existingScript = document.querySelector(
+  //     'script[src*="paypal.com/sdk/js"]'
+  //   );
+  //   if (existingScript) {
+  //     existingScript.remove();
+  //   }
+  // }, []);
+
   return (
     <PayPalScriptProvider
       options={{
         clientId: NEXT_PUBLIC_APP_PAYPAL_CLIENT_ID,
         currency: "USD",
-        intent: "capture", 
-        commit: true,   
-      }}>
+        intent: "capture",
+        commit: true,
+      }}
+    >
       <PayPalButtons
         style={{
           layout: "vertical",
@@ -24,13 +39,32 @@ const PayPalCheckout = (data: ICreateOrder) => {
         }}
         createOrder={async () => {
           const res: any = await PayPalCreateOrderAPI(data);
-          return res.order_id; // return the PayPal order ID
+          console.log(res)
+          if (res.approval_url) {
+            window.location.href = res.approval_url; 
+          }
+          return res.order_id;
         }}
         onApprove={async (data, actions) => {
-          // order capture logic here
+          try {
+            const res = await fetch(`https://bo.tamasha.me/api/paypal/capture-order/${data.orderID}`, {
+              method: "POST",
+            });
+
+            const result = await res.json();
+
+            if (result.status === "success") {
+              window.location.href = "https://bo.tamasha.me/api/paypal/payment-success";
+            } else {
+              window.location.href = "https://bo.tamasha.me/api/paypal/payment-cancel";
+            }
+          } catch (error) {
+            console.error("Capture error:", error);
+            window.location.href = "/subscriptions/error";
+          }
         }}
         onError={(error) => {
-          console.log(error);
+          console.error("PayPal Error:", error);
         }}
       />
     </PayPalScriptProvider>
